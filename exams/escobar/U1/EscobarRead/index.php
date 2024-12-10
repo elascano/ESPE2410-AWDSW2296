f34fec8f2bc9715ead4b1234f8383a93c1c1c93a
Isaac Alejandro Escobar Rivadeneira <iaescobar3@espe.edu.ec>
2024-12-10 12:41:41 -0500

    upgrade

---
+++ exams/escobar/U1/EscobarRead/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu</title>
+    <link href="stile.css" rel="stylesheet" type="text/css">
+    <link href="estilo_barra.css" rel="stylesheet" type="text/css">
+    <link href="footer.php" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<?php include 'navegation.php'; ?>
+
+    <header style="text-align: left; padding: 2px;">
+        <img src="logo.jpg" alt="Header Image" style="max-width: 4%; height: auto;">
+    </header>
+
+    
+    <hr size="10" width="100%" noshade color="blue">
+    <hr size="10" width="100%" noshade color="#2F0CEC">
+    <h1>Information of Team</h1>
+    
+    <p><strong>Programer: Isaac Escobar</strong>    </p>
+    <hr size="10" width="100%" noshade color="#2F0CEC">
+
+    <h1>Information</h1>
+    <p><strong>Read to the Team of the premier League</strong></p>
+    <pre>
+To the diferent team to compite of the cup and what team are the best in the world
+
+    </pre>
+    <hr size="10" width="100%" noshade color="blue">
+
+   
+    <p>Support Team</p>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
