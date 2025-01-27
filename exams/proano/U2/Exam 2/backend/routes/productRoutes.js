c127318008331587bb872f7dd9bff7ba82dc3a63
JoseProano <96480862+JoseProano@users.noreply.github.com>
2025-01-27 13:49:28 -0500

    Exam 2
    Exam 2

---
+++ exams/proano/U2/Exam 2/backend/routes/productRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const {
+  getProductById,
+} = require('../controllers/productController');
+
+const router = express.Router();
+
+router.get('/:id', getProductById);
+
+module.exports = router;
