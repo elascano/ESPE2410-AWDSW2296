ae8f790b02f04725c416c9a76fa75417b6b2da49
Asmalkevin1999 <kdasmal@espe.edu.ec>
2025-02-23 22:25:49 -0500

    WS,HW

---
+++ hw/asmal/U3/HW16, 17, 18/app/Http/Controllers/ProductController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json($products, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric'
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product)
+            return response()->json(
+                [
+                    'message' => 'Product not found'
+                ],
+                404
+            );
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
